0f464144b1f8c1e5fd7b4ed92c1bd98730844268
Timon Vonk <timonv@gmail.com>
2015-03-02 21:50:57 +0100

    Fetch and store token

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,94 @@
 #![feature(io)]
+#![feature(old_io)]
+#![feature(fs)]
+#![feature(rustc_private)] // TODO Migrate to crates.io variant (json)
 
 extern crate hyper;
 extern crate regex;
+extern crate serialize;
 
 
 // TODO
 fn main() {
-    let token = authentication::get_oauth_token_or_panic();
+    let (token,_guard) = authentication::get_oauth_token_or_panic();
     println!("{}", token);
 }
 
 // Temp
 mod authentication {
     // Authentication
-    use std::old_io::net::ip::Ipv4Addr;
-    use hyper::server::{Server, Request, Response};
+    use std::net::IpAddr;
+    use hyper::server::{Server, Request, Response, Listening};
     use hyper::uri::RequestUri::AbsolutePath;
+    use hyper::client::{Client};
     use std::old_io::Command;
-    // use hyper::status;
     use regex::Regex;
     use std::sync::mpsc::channel;
     use std::sync::Mutex;
+    use std::io::prelude::*;
+    use std::thread;
+    use serialize::json;
+    use std::fs;
 
-    type AuthCode = String;
+    type AuthToken = String;
+    type TempCode = String;
 
-    pub fn get_oauth_token_or_panic() -> AuthCode {
-        Command::new("xdg-open").arg("https://slack.com/oauth/authorize?client_id=2334733471.3592055147").output().unwrap();
-        let server = Server::http(Ipv4Addr(127, 0, 0, 1), 9999);
+    const AUTH_TOKEN_FILE: &'static str = "auth_token";
+    const SLACK_CLIENT_ID: &'static str = "2334733471.3592055147";
+    const SLACK_CLIENT_SECRET: &'static str = "37721a57d17018b206fb1264caa7d707";
+
+    pub fn get_oauth_token_or_panic() -> (AuthToken, Option<Listening>) {
+        match maybe_existing_token(AUTH_TOKEN_FILE) {
+            Some(token) => (token, None),
+            None => {
+                let (token, listener) = arrange_new_token();
+                store_token(&token);
+                (token, Some(listener))
+            }
+        }
+    }
+
+    fn maybe_existing_token(token_file: &str) -> Option<AuthToken> {
+        match fs::File::open(token_file) {
+            Ok(mut file) => {
+                let mut s = String::new();
+                file.read_to_string(&mut s).unwrap();
+
+                if !s.is_empty() {
+                    Some(s)
+                } else {
+                    None }
+            },
+            Err(_) => None
+        }
+    }
+
+    fn arrange_new_token() -> (AuthToken, Listening) {
+        let (temp_code, listener) = request_temp_code();
+        let token = request_token(&temp_code);
+        (token, listener)
+    }
+
+    fn store_token(token: &AuthToken) {
+        let mut f = fs::File::create(AUTH_TOKEN_FILE).unwrap();
+        f.write_all(token.as_bytes()).unwrap();
+    }
+
+    fn request_temp_code() -> (TempCode, Listening) {
+        Command::new("xdg-open").arg(format!("https://slack.com/oauth/authorize?client_id={}", SLACK_CLIENT_ID)).output().unwrap();
 
         let (tx, rx) = channel();
         let mtx = Mutex::new(tx);
 
-        let mut _guard = server.listen(move |req: Request, res: Response| {
+        let server = Server::http(move |req: Request, res: Response| {
             match req.uri {
-                AbsolutePath(path) => {
-                    let authcode = extract_auth_code(&path).unwrap();
-                    mtx.lock().unwrap().send(authcode).unwrap();
+                AbsolutePath(ref path) => {
+                    match extract_temp_code(&path) {
+                        Some(tempcode) =>  {
+                            mtx.lock().unwrap().send(tempcode).unwrap();
+                        },
+                        None => ()
+                    }
                 },
                 _ => ()
             }
@@ -43,39 +96,75 @@ mod authentication {
             let mut res = res.start().unwrap();
             res.write_all(b"Thanks! Please return to Lax").unwrap();
             res.end().unwrap();
-        }).unwrap();
+        });
 
-        let authcode = rx.recv().unwrap();
-        _guard.close().unwrap();
-        authcode
+        let mut guard = server.listen(IpAddr::new_v4(127, 0, 0, 1), 9999).unwrap();
+
+        let tempcode = rx.recv().unwrap();
+        guard.close().unwrap();
+        (tempcode, guard)
     }
 
-    fn extract_auth_code(path: &str) -> Result<AuthCode, &'static str> {
-        let re = Regex::new(r"code=(.+?)(&|$)").unwrap();
-        // Alternative example with map:
-        // re.captures(path).map_or(Err("Expected authentication code"),
-        //     |captures| try!(captures.at(1)).to_string())
-        //
-        //  or:
-        //
-        // re.captures(path).and_then(
-        //   |captures| try!(captures.at(1)).to_string())
-        // .or(Err("Expected authentication code")
-        // 
-        match re.captures(path) {
-            Some(captures) => Ok(captures.at(1).unwrap().to_string()),
-            None => Err("Expected authentication code.")
+    fn request_token(temp_code: &TempCode) -> AuthToken {
+        let mut client = Client::new();
+        // I thought & is sufficient to make it a slice
+        let mut res = client.get(format_access_uri(temp_code).as_slice()).send().unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        match json::from_str(&body) {
+            Ok(json) => {
+                match json.find("access_token") {
+                    Some(j) => j.as_string().unwrap().to_string(),
+                    _ =>  panic!("Unexpected json in slack response\n{}", json.pretty())
+                }
+            },
+            _ => panic!("Reponse not json")
         }
+    }
+
+    fn format_access_uri(temp_code: &TempCode) -> String {
+        let base = "https://slack.com/api/oauth.access";
+        let query = format!("?client_id={}&client_secret={}&code={}", SLACK_CLIENT_ID, SLACK_CLIENT_SECRET, temp_code);
+        format!("{}{}", base, query)
+    }
+
+    fn extract_temp_code(path: &str) -> Option<TempCode> {
+        let re = Regex::new(r"code=(.+?)(&|$)").unwrap();
+        re.captures(path).map(|cap| cap.at(1).unwrap().to_string())
     }
 
     #[cfg(test)]
     mod tests {
-        use super::extract_auth_code;
+        use super::extract_temp_code;
+        use super::maybe_existing_token;
+        const AUTH_TOKEN_FILE: &'static str = "auth_token_test";
 
         #[test]
-        fn test_extract_auth_code() {
+        fn test_extract_temp_code() {
             let path = "www.timonv.nl?code=blablabla";
-            assert_eq!(extract_auth_code(path).unwrap(), "blablabla".to_string())
+            assert_eq!(extract_temp_code(path).unwrap(), "blablabla".to_string())
+        }
+
+        #[test]
+        fn test_maybe_existing_token() {
+            // None if file doesn't exist
+            fs::remove_file(AUTH_TOKEN_FILE);
+            assert_eq!(maybe_existing_token(AUTH_TOKEN_FILE), None);
+
+            // Some if file exists
+            let mut f = fs::File::create(AUTH_TOKEN_FILE).unwrap();
+            f.write_all(b"123").unwrap();
+
+            assert_eq!(maybe_existing_token(AUTH_TOKEN_FILE), Some("123".to_string()));
+
+            // None if file exists but empty
+            let mut f = fs::File::create(AUTH_TOKEN_FILE).unwrap();
+            f.write_all(b"").unwrap();
+
+            assert_eq!(maybe_existing_token(AUTH_TOKEN_FILE), None);
+
+            // Cleanup
+            fs::remove_file(AUTH_TOKEN_FILE);
         }
     }
 }
